5d41ec0cba02faa235b6edb626366a5a0f714c77
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2023-12-29 05:01:42 +0330

    revise

--- src/Components/File.jsx
+++ src/Components/File.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import uploadedFile from "../utils/uploadedFile";
 import { convertSize } from "../utils/convertSize";
 export default function File({ file }) {
